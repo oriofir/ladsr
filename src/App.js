44afb9213cebe31b1afcde2ca875f20067e0fee5
oriofir <97814056+oriofir@users.noreply.github.com>
2023-08-10 18:53:48 -0700

    fixed title and favcon and the kickstarter button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./LADSR_LOGO_ROC_HORZ_3.png";
+// import LoopingVideo from "./Components/LoopingVideo";
 import "./App.css";
 
 function App() {
   return (
     <>
+      {/* <LoopingVideo /> */}
       <div className="comingSoon">SITE COMING SOON...</div>
       <div className="outerContainer">
         <img src={logo} className="logoImage" alt="logo" />
-        <a href="https://example.com" className="kickstarterButton">
+        <a
+          href="https://www.kickstarter.com/projects/1837758836/ladsr-synth-library-shop-and-educational-center"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="kickstarterButton"
+        >
           KICKSTARTER
         </a>
       </div>
