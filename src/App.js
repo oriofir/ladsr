ca3519f0ca395ea72dd1c2229dd9607abd0074ab
oriofir <97814056+oriofir@users.noreply.github.com>
2023-08-22 12:13:03 -0700

    added welcome button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from "./assets/LADSR_LOGO_ROC_HORZ_YELLOW.png";
 import Main from "./Components/Main.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="outerContainer">
-        <img src={logo} className="logoImage" alt="logo" />
-
-        <Main />
-      </div>
+      <Main />
     </>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ModularVid from "../assets/ModularVid.mp4"
+import logo from "../assets/LADSR_LOGO_ROC_HORZ_YELLOW.png";
 
 const Main = () => {
   return (
   <div className="main">
+    <div className="overlay"></div>
     <video src={ModularVid} autoPlay loop muted />
+    <img src={logo} className="logoImage" alt="logo" />
     <div className="content">
-      <h1>Welcome</h1>
+      <button className="welcomeButton">WELCOME</button>
+      
     </div>
   </div>
   )
