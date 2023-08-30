08b8a8c7b20d0fb6c5d6aacbf8bb7ed6aab4de1a
oriofir <97814056+oriofir@users.noreply.github.com>
2023-08-30 13:58:08 -0700

    created a link from the welcome button to a home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Main from "./Components/Main.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./Components/Landing.jsx";
+import Home from "./Components/Home.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Main />
+      <Router>
+        <Routes>
+          <Route path="/" Component={Landing} />
+          <Route exact path="/home" Component={Home} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import {Link} from "react-router-dom"
 import ModularVid from "../assets/ModularVid.mp4"
 import logo from "../assets/LADSR_LOGO_ROC_HORZ_YELLOW.png";
 
-const Main = () => {
+const Landing = () => {
   return (
   <div className="main">
     <div className="overlay"></div>
     <video src={ModularVid} autoPlay loop muted />
     <img src={logo} className="logoImage" alt="logo" />
     <div className="content">
+      <Link to="/home">
       <button className="welcomeButton">WELCOME</button>
-      
+      </Link>
     </div>
   </div>
   )
 }
 
-export default Main
+export default Landing
