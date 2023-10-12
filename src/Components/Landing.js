8c7a038ada292a27733011e1495c66e98a46b3c8
oriofir <97814056+oriofir@users.noreply.github.com>
2023-10-12 16:43:23 -0700

    css fixes to landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -6,15 +6,18 @@ import logo from "../assets/LADSR_LOGO_ROC_HORZ_YELLOW.png";
 
 const Landing = () => {
   return (
-    <div className="main">
+    <>
       <div className="overlay"></div>
       <video src={ModularVid} className="synthVid" autoPlay loop muted></video>
-      <img src={logo} className="logoImage" alt="logo" />
-
-      <Link to="/home" className="button-class">
-        <button className="welcomeButton">WELCOME</button>
-      </Link>
-    </div>
+      <div className="main">
+        <img src={logo} className="logoImage" alt="logo" />
+        <div className="buttonContainer">
+          <Link to="/home" className="button-class">
+            <button className="welcomeButton">WELCOME</button>
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
