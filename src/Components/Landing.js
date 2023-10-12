b7a6df6517e626d4fbbd93a4611b9ff4d7a60374
oriofir <97814056+oriofir@users.noreply.github.com>
2023-10-12 15:03:07 -0700

    fixed css

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {Link} from "react-router-dom"
-import ModularVid from "../assets/ModularVid.mp4"
+import { Link } from "react-router-dom";
+import ModularVid from "../assets/ModularVid.mp4";
+
 import logo from "../assets/LADSR_LOGO_ROC_HORZ_YELLOW.png";
 
 const Landing = () => {
   return (
-  <div className="main">
-    <div className="overlay"></div>
-    <video src={ModularVid} autoPlay loop muted />
-    <img src={logo} className="logoImage" alt="logo" />
-    <div className="content">
+    <div className="main">
+      <div className="overlay"></div>
+      <video src={ModularVid} className="synthVid" autoPlay loop muted></video>
+      <img src={logo} className="logoImage" alt="logo" />
+
       <Link to="/home" className="button-class">
-      <button className="welcomeButton">WELCOME</button>
+        <button className="welcomeButton">WELCOME</button>
       </Link>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
